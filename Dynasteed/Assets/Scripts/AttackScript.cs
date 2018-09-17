4b10f3a03578e75b0a60bc06c4b3cd792e828513
Rilogator <jhowell713@gmail.com>
2018-09-17 13:07:08 -0400

    Damge code added

---
+++ Dynasteed/Assets/Scripts/AttackScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using RoboRyanTron.Unite2017.Variables;
+
+public class AttackScript : MonoBehaviour
+{
+	private Animator anim;
+	//private Rigidbody2D myBody;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown("Fire1"))
+		{
+			anim.SetTrigger("Attack");
+		}
+	}
+
+	#region Animator Events
+
+	public void ResetTrigger(string trigger)
+	{
+		anim.ResetTrigger(trigger);
+	}
+
+	public void SetCombo(bool state)
+	{
+		anim.SetBool("canCombo", state);
+	}
+
+	#endregion
+}
---
+++ Dynasteed/Assets/Scripts/SidescrollerMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using RoboRyanTron.Unite2017.Variables;
+
+public class SidescrollerMovement : MonoBehaviour
+{
+	public FloatReference maxSpeed;
+	public FloatReference accel;
+
+	private Rigidbody2D myBody;
+
+	private float _moveX, _moveY;
+	private Vector2 movement;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start ()
+	{
+		myBody = this.GetComponent<Rigidbody2D>();
+		anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		_moveX = Input.GetAxisRaw("Horizontal");
+		movement = new Vector3(_moveX, _moveY).normalized;
+	}
+
+	private void FixedUpdate()
+	{
+		Move();
+	}
+
+	public void Move()
+	{
+		if ((myBody.velocity.x > maxSpeed && _moveX < 0)
+			|| (myBody.velocity.x < -maxSpeed && _moveX > 0)
+			|| Mathf.Abs(myBody.velocity.x) < maxSpeed)
+			myBody.AddForce(new Vector3(movement.x, 0) * accel * Time.deltaTime);		
+	}
+}
